c88dcdc8989272ce514f92cd8c1130ad86b27c31
Miles Pratt <miles@thoughtpowder.com>
2019-12-08 18:29:59 -0500

    add env

--- src/pages/tours.jsx
+++ src/pages/tours.jsx
@@ -1,13 +1,20 @@
 import React from "react"
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
-
+import styles from "../css/tour.module.css"
 import { graphql } from "gatsby"
 
 export default function tours({ data }) {
   return (
     <Layout>
       <StyledHero img={data.tourBcg.childImageSharp.fluid} />
+      {data.allContentfulTour.nodes.map(tour => {
+        return (
+          <div className={styles.tour} key={tour.id}>
+            {tour.name}
+          </div>
+        )
+      })}
     </Layout>
   )
 }
@@ -19,6 +26,21 @@ export const query = graphql`
         fluid(quality: 90, maxWidth: 4160) {
           ...GatsbyImageSharpFluid_tracedSVG
         }
+      }
+    }
+    allContentfulTour {
+      nodes {
+        name
+        price
+        country
+        start
+        days
+        journey {
+          day
+          info
+          id
+        }
+        id
       }
     }
   }
